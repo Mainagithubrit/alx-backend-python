3ca1164efd62db1ad38d938aad701f8b1ad2bc5a
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-10-18 21:57:12 +0300

    Task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" A coroutine"""
+
+import random
+import asyncio
+from typing import Generator
+
+
+async def async_generator():
+    """A coroutine that loops 10 times,
+    and waits for 1 second before printing each value"""
+    for _ in range(0, 10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
