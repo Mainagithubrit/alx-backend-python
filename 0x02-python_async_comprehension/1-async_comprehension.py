87b0ed3781b81cab40af4abdd64225d4194a9d0d
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-10-18 22:36:02 +0300

    Task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,13 +1,15 @@
-#!/usr/bin/env pyrhon3
-
-"""A module"""
+#!/usr/bin/env python3
+'''A module.
+'''
 from typing import List
 from importlib import import_module as using
+
+
 async_generator = using('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """A coroutine that collects 10 random numbers,
-    using an async comprehension"""
-
-    return [n async for n in async_generator()]
+    '''Creates a list of 10 numbers from,
+    a 10-number generator.
+    '''
+    return [num async for num in async_generator()]
