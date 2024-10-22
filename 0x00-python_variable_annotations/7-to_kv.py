64168b273d5684a145ceffef12c3875c91e39525
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-10-22 21:38:40 +0300

    Task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""A type annotated function"""
+
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """A function that returns a tuple"""
+    return (k, float(v ** 2))
